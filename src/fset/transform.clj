08b214dcc5f43d2f1049cdaa0bf7ada103d71f8c
Jan Christopher Roßbach <jan.rossbach@hhu.de>
2021-09-17 17:48:08 +0200

    Remove definitions in transform

--- src/fset/transform.clj
+++ src/fset/transform.clj
@@ -1,23 +1,10 @@
 (ns fset.transform
   (:require
-   [com.rpl.specter :as s]
-   [lisb.core :refer [eval-ir-formula]]))
+   [fset.util :as util]))
 
 ;; Pure Namespace which does the unraveling of finite sets on the immediate representation of lisb.
 
-(defn get-node-by-tag
-  [el-tag ir]
-  (s/select [(s/walker #(= (:tag %) el-tag))] ir))
-
-(defn update-node-by-tag
-  [el-tag update-fn m]
-  (s/transform [:ir (s/walker #(= (:tag %) el-tag))] update-fn m))
-
 (defn transform-sets
-  [m]
-  m)
-
-(defn transform-definitions
   [m]
   m)
 
@@ -33,6 +20,5 @@
   [m]
   (->> m
        (transform-sets)
-       (transform-definitions)
        (transform-invariant)
        (transform-operations)))
--- test/fset/transform_test.clj
+++ test/fset/transform_test.clj
@@ -1,1 +1,4 @@
-(ns fset.transform-test)
+(ns fset.transform-test
+  (:require
+   [fset.transform :refer :all]
+   [clojure.test :refer :all]))
