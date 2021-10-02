a7991c5e87dffc7a55f19756ee2e0b1cdba827b2
Jan Christopher Roßbach <jan.rossbach@hhu.de>
2021-10-02 09:00:01 +0200

    Remove guessing function until actually needed

--- src/fset/core.clj
+++ src/fset/core.clj
@@ -3,17 +3,10 @@
    [clojure.spec.alpha :as spec]
    [fset.spec :refer :all]
    [fset.util :as util]
-   [lisb.core :refer [eval-ir-formula]]
    [lisb.translation.util :refer [lisb->ir ir->ast ir->b]]
    [lisb.prob.animator :refer [state-space!]]))
 
 ;; High level public interface of the app. Ties together the lisb,  config and transform namespaces to provide all required functionality.
-
-;; FIXME
-(defn- get-set-elements
-  [set-identifier machine]
-  (let [{:keys [ir ss meta]} machine]
-    (eval-ir-formula (lisb->ir `(bcomp-set [:x] (bmember? :x ~set-identifier))))))
 
 (defn make-mch!
   [lisb meta]
