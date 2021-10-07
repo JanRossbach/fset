cbbda2cd2b1028f679890eff999695dd73038b7b
Jan Christopher Roßbach <jan.rossbach@hhu.de>
2021-10-07 12:37:16 +0200

    Setup CI tools for gitlab

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [org.clojure/clojure "1.10.1"]
                  [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.48.0"]]
+  :plugins [[lein-cloverage "1.2.2"]]
   :repl-options {:init-ns fset.core})
