82a303d8648c3d71c86480f2731c4a1c4fb917f9
Jan Christopher Roßbach <jan.rossbach@hhu.de>
2022-03-02 15:59:48 +0100

    Add Simplification Tests

--- components/simplifier/test/hhu/fset/simplifier/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns hhu.fset.simplifier.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [hhu.fset.simplifier.interface :as simplifier]))
-
-(deftest dummy-test
-  (is (= 1 1)))
